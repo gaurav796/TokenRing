7ce7f9e0002b3ec5ffe1445575bcdfceff1b61c4
GAURAV KUMAR <kumar.gauravnitk@gmail.com>
2015-01-07 17:24:59 +0100

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,49 @@
+import org.apache.xmlrpc.XmlRpcException;
+import org.apache.xmlrpc.server.PropertyHandlerMapping;
+import org.apache.xmlrpc.server.XmlRpcServer;
+import org.apache.xmlrpc.server.XmlRpcServerConfigImpl;
+import org.apache.xmlrpc.webserver.WebServer;
+
+import java.io.IOException;
+
+
+public class Server implements Runnable {
+
+	//TODO 
+	private int port;
+
+	public Server(int port)
+	{
+
+		this.port = port;
+	}
+	//END CHANGE
+    @Override
+    public void run() {
+        WebServer webServer = new WebServer(this.port); //TODO
+
+        XmlRpcServer xmlRpcServer = webServer.getXmlRpcServer();
+
+        PropertyHandlerMapping phm = new PropertyHandlerMapping();
+
+        try {
+            phm.addHandler("Calculator", Calculator.class);
+            phm.addHandler("Node", Node.class);
+            phm.addHandler("TokenRing", TokenRing.class);
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+
+        xmlRpcServer.setHandlerMapping(phm);
+        XmlRpcServerConfigImpl serverConfig =
+                (XmlRpcServerConfigImpl) xmlRpcServer.getConfig();
+        serverConfig.setEnabledForExtensions(true);
+        serverConfig.setContentLengthOptional(false);
+
+        try {
+            webServer.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
