31dde8cb9130dfddc7a09489d1b4db16c914faff
GAURAV KUMAR <kumar.gauravnitk@gmail.com>
2015-01-07 17:21:54 +0100

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,52 @@
+//auxiliary class for implementing of XMLRPC-methods
+
+import java.io.*;
+import java.util.Vector;
+
+public class Calculator {
+
+	public int result_update(String result)
+	{
+	System.out.println(" Result is updating..");
+	PrintWriter out = null;
+    try {
+        out = new PrintWriter(new BufferedWriter(new FileWriter("Calculator.txt", true)));
+        out.println(result);
+    } catch (IOException e) {
+        System.err.println(e);
+    } finally {
+        if (out != null) {
+            out.close();
+        }
+    }
+        System.out.println("Update Done!");
+	     return 42;
+	}
+    
+	    public Vector<String> getList() {
+        System.out.println("Returning list of all events...");
+        Vector<String> result = new Vector<String>();
+        File file = new File("Calculator.txt");
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line;
+        try {
+            while ((line = br.readLine()) != null) {
+                result.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("List Passed!");
+        return result;
+    }
+}
